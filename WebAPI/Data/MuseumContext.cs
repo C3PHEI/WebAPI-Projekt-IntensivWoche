696c2b0759cd5d06060df6e9a42bbbb102db5a1e
Maximilian Läpple <m.max32@yahoo.com>
2023-12-12 21:03:26 +0100

    Projektdateien hinzufügen.

---
+++ WebAPI/Data/MuseumContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+public class MuseumContext : DbContext
+{
+    public DbSet<MuseumArea> MuseumAreas { get; set; }
+    public DbSet<VisitorEntry> VisitorEntries { get; set; }
+    public DbSet<VisitorExit> VisitorExits { get; set; }
+
+    public MuseumContext(DbContextOptions<MuseumContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Model configuration code goes here
+    }
+}
---
+++ WebAPI/Models/MuseumArea.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Models
+{
+    public class MuseumArea
+    {
+        public int AreaID { get; set; }
+        public required string AreaName { get; set; }
+        public int MaxVisitors { get; set; }
+        public TimeSpan OpeningTime { get; set; }
+        public TimeSpan ClosingTime { get; set; }
+    }
+}
